e4a21de74cbd01de2db0715ac2290ec3783575cc
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-04-26 09:55:27 +0530

    PL/SQL syntax complete

--- D4_PratikSatpute_62758_Q1.sql
+++ D4_PratikSatpute_62758_Q1.sql
@@ -1,14 +1,22 @@
 
 Drop procedure if exists sp_digitsum;
 
-DELIMITER$$
+DELIMITER $$
 
 create procedure sp_digitsum(sp_num INT)
 BEGIN
+declare sp_var INT Default sp_num;
+declare sp_sum INT Default 0;
+declare sp_add INT;
 
-    
+while sp_var!= 0 
+Do 
+set sp_add= FLOOR(sp_var%10);
+set sp_sum=sp_sum + sp_add;
+set sp_var=FLOOR(sp_var/10);
+END while;
+insert into temp values(sp_num,sp_sum); 
 
-END
-
+END;
 $$
-DELIMITER;
+DELIMITER ;
